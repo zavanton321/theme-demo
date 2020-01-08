3e2007bbbc3ee2135a8c3be066dcb53296c5026c
Anton Zavyalov <zavanton123@gmail.com>
2020-01-08 17:04:20 +0300

    custom style for snackbar

--- app/src/main/java/ru/zavanton/startdemo/MainActivity.kt
+++ app/src/main/java/ru/zavanton/startdemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package ru.zavanton.startdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInfo.setOnClickListener {
+
+            Snackbar.make(tvInfo, "Hello world", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
