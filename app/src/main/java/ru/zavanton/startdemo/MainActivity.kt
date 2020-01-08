2b5b6cf9ff0d137fc34335da1b1fe6e7168daaa5
Anton Zavyalov <zavanton123@gmail.com>
2020-01-08 17:29:39 +0300

    simple night mode

--- app/src/main/java/ru/zavanton/startdemo/MainActivity.kt
+++ app/src/main/java/ru/zavanton/startdemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.zavanton.startdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInfo.setOnClickListener {
+            when (AppCompatDelegate.getDefaultNightMode()) {
+                MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                else -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
     }
 }
